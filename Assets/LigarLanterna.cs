4ef5ddd014826a4602678bc9bf78af78d6aa28e5
Rodrigo Rafael <rodrigorafaeldamaceno@gmail.com>
2019-09-07 13:46:13 -0300

    added more lights

---
+++ Assets/LigarLanterna.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LigarLanterna : MonoBehaviour
+{
+    public Light[] lights;
+	public KeyCode keyboard;
+
+	void Update ()
+	{
+		foreach (Light light in lights)
+		{
+			if (Input.GetKeyDown(keyboard))
+			{
+				light .enabled = !light .enabled;
+			}
+		}
+	}
+}
---
+++ Assets/scripts/LigarSol.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LigarSol : MonoBehaviour
+{
+    public Light[] lights;
+	public KeyCode keyboard;
+
+	void Update ()
+	{
+		foreach (Light light in lights)
+		{
+			if (Input.GetKeyDown(keyboard))
+			{
+				light .enabled = !light .enabled;
+			}
+		}
+	}
+}
