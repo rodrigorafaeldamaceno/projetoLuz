f25c0c40c53ceb3a4cf3fa91fafd1ba23db443b8
Rodrigo Rafael <rodrigorafaeldamaceno@gmail.com>
2019-09-08 20:31:19 -0300

    adicionada função de piscar

---
+++ Assets/scripts/pisca.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pisca : MonoBehaviour
+{
+     
+    private float timer = 1;
+    public KeyCode keyboard;
+    public Light[] lights;
+		private bool status = true;
+   
+   void Update () { 
+      if (Input.GetKeyDown(keyboard)){
+				status = !status;
+			}
+			if(status){
+				timer -= Time.deltaTime;
+				foreach (Light light in lights){
+					if(timer < 0){
+						light.enabled = !light.enabled;
+						timer = 0.2f; // o segredo esta aqui xD
+					}
+				}
+			}
+   }
+}
+
